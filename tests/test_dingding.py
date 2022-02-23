b1dfc4a6d1ebf594d205ef37b1858c71546a4fb8
qiusheng2011 <137812291@qq.com>
2022-02-23 20:10:54 +0800

    update

---
+++ tests/test_dingding.py
@@ -0,0 +1,76 @@
+
+from unittest import TestCase
+from dingding.dingding import DingDingGroupRobot, TextMessage, LinkMessage, MarkdownMessage, BasicMessage, ActionCardMessage, MultiActionCardMessage
+import asyncio
+
+
+class DingDingGroupRobotUnittest(TestCase):
+
+    def setUp(self) -> None:
+        self.baseurl = "https://oapi.dingtalk.com"
+        self.access_token = "a6a968e78f9052a2284066589babd470d4d1eafd89d16fd9ff41622879c3cd31"
+        self.group_robot = DingDingGroupRobot(baseurl=self.baseurl, access_token=self.access_token)
+        self.loop = asyncio.get_event_loop()
+        return super().setUp()
+
+    def test_textmsg(self):
+
+        msg = TextMessage()
+        msg.text.content = "TextMessage"
+        msg.at.atMobiles = ["18513279524"]
+        msg.at.isAtAll = False
+
+        
+        status, msg =  self.loop .run_until_complete(self.group_robot.async_send_group_message(msg))
+        self.assertTrue(status, msg)
+
+    def test_linkmsg(self):
+        msg = LinkMessage()
+        msg.link.title = "LinkMessage"
+        msg.link.text = "LinkMessage"
+        msg.link.picUrl= "https://s3.bmp.ovh/imgs/2022/02/ddf12e19b1e8ee60.png"
+        msg.link.messageUrl = "https://www.aliyun.com"
+        
+        status, msg = self.loop .run_until_complete(self.group_robot.async_send_group_message(msg))
+        self.assertTrue(status, msg)
+    
+    def test_markdown(self):
+        msg = MarkdownMessage()
+        msg.markdown.title = "MarkdownMessage"
+        msg.markdown.text  = "# MarkdownMessage \n ## MarkdownMessage"
+        
+        status, msg = self.loop.run_until_complete(self.group_robot.async_send_group_message(msg))
+        self.assertTrue(status, msg)
+        
+    def test_actioncard(self):
+        
+        msg = ActionCardMessage()
+        msg.actionCard.title = "ActionCardMessage"
+        msg.actionCard.text = "ActionCardMessage"
+        msg.actionCard.singleTitle = "ActionCardMessage.singleTitle"
+        msg.actionCard.singleUrl = "https://www.aliyun.com"
+        
+        status, msg = self.loop.run_until_complete(self.group_robot.async_send_group_message(msg))
+        self.assertTrue(status, msg)
+    
+    def test_multiactioncard(self):
+        msg = MultiActionCardMessage()
+        msg.actionCard.title = "ActionCardMessage"
+        msg.actionCard.text = "ActionCardMessage"
+        msg.actionCard.btnOrientation = "1"
+        msg.actionCard.btns.append(MultiActionCardMessage.ActionCard.Btns(title="title1",actionUrl="https://www.aliyun.com"))
+        msg.actionCard.btns.append(MultiActionCardMessage.ActionCard.Btns(title="title2",actionUrl="https://www.aliyun.com"))
+        
+        status, msg = self.loop.run_until_complete(self.group_robot.async_send_group_message(msg))
+        self.assertTrue(status, msg)
+        
+        
+        msg = MultiActionCardMessage()
+        msg.actionCard.title = "ActionCardMessage"
+        msg.actionCard.text = "ActionCardMessage"
+        msg.actionCard.btnOrientation = "0"
+        msg.actionCard.btns.append(MultiActionCardMessage.ActionCard.Btns(title="title1",actionUrl="https://www.aliyun.com"))
+        msg.actionCard.btns.append(MultiActionCardMessage.ActionCard.Btns(title="title2",actionUrl="https://www.aliyun.com"))
+        
+        status, msg = self.loop.run_until_complete(self.group_robot.async_send_group_message(msg))
+        self.assertTrue(status, msg)
